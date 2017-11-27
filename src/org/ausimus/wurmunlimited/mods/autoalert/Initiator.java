f2e3af3b1a2e2c75a9d795697006f7237c9b9661
ausimus <ausimus1@gmail.com>
2017-11-27 11:43:47 -0500

    Bugfixes

--- src/org/ausimus/wurmunlimited/mods/autoalert/Initiator.java
+++ src/org/ausimus/wurmunlimited/mods/autoalert/Initiator.java
@@ -1,3 +1,17 @@
+/*
+     ___          ___          ___                     ___          ___          ___
+    /\  \        /\__\        /\  \         ___       /\__\        /\__\        /\  \
+   /::\  \      /:/  /   2   /::\  \    0  /\  \  1  /::|  |  7   /:/  /       /::\  \
+  /:/\:\  \    /:/  /       /:/\ \  \      \:\  \   /:|:|  |     /:/  /       /:/\ \  \
+ /::\~\:\  \  /:/  /  ___  _\:\~\ \  \     /::\__\ /:/|:|__|__  /:/  /  ___  _\:\~\ \  \
+/:/\:\ \:\__\/:/__/  /\__\/\ \:\ \ \__\ __/:/\/__//:/ |::::\__\/:/__/  /\__\/\ \:\ \ \__\
+\/__\:\/:/  /\:\  \ /:/  /\:\ \:\ \/__//\/:/  /   \/__/~~/:/  /\:\  \ /:/  /\:\ \:\ \/__/
+     \::/  /  \:\  /:/  /  \:\ \:\__\  \::/__/          /:/  /  \:\  /:/  /  \:\ \:\__\
+     /:/  /    \:\/:/  /    \:\/:/  /   \:\__\         /:/  /    \:\/:/  /    \:\/:/  /
+    /:/  /      \::/  /      \::/  /     \/__/        /:/  /      \::/  /      \::/  /
+    \/__/        \/__/        \/__/                   \/__/        \/__/        \/__/
+*/
+
 package org.ausimus.wurmunlimited.mods.autoalert;
 
 import com.wurmonline.server.Players;
@@ -19,38 +33,38 @@ import java.util.concurrent.TimeUnit;
 public class Initiator  implements WurmServerMod, ServerPollListener, Configurable {
     private Properties prop = new Properties();
 
-    private long sysTime = System.currentTimeMillis();
+    private String dir = "mods/AutoAlerts.properties";
 
     private static boolean transmitTwitter;
 
-    private long lastPoll0 = 0;
+    private long lastPoll0;
     private long Seconds0;
 
-    private long lastPoll01 = 0;
+    private long lastPoll01;
     private long Seconds01;
 
-    private long lastPoll02 = 0;
+    private long lastPoll02;
     private long Seconds02;
 
-    private long lastPoll03 = 0;
+    private long lastPoll03;
     private long Seconds03;
 
-    private long lastPoll04 = 0;
+    private long lastPoll04;
     private long Seconds04;
 
-    private long lastPoll05 = 0;
+    private long lastPoll05;
     private long Seconds05;
 
-    private long lastPoll06 = 0;
+    private long lastPoll06;
     private long Seconds06;
 
-    private long lastPoll07 = 0;
+    private long lastPoll07 ;
     private long Seconds07;
 
-    private long lastPoll08 = 0;
+    private long lastPoll08;
     private long Seconds08;
 
-    private long lastPoll09 = 0;
+    private long lastPoll09;
     private long Seconds09;
 
     private boolean useAlert0;
@@ -103,7 +117,7 @@ public class Initiator  implements WurmServerMod, ServerPollListener, Configurab
         useAlert08 = Boolean.parseBoolean(properties.getProperty("useAlert08", Boolean.toString(useAlert08)));
 
         Seconds09 = Long.parseLong(properties.getProperty("Seconds09", Long.toString(Seconds09)));
-        useAlert09 = Boolean.parseBoolean(properties.getProperty("useAlert08", Boolean.toString(useAlert09)));
+        useAlert09 = Boolean.parseBoolean(properties.getProperty("useAlert09", Boolean.toString(useAlert09)));
 
         useDeedAlert = Boolean.parseBoolean(properties.getProperty("useDeedAlert", Boolean.toString(useDeedAlert)));
         DA_pollSeconds = Long.parseLong(properties.getProperty("DA_pollSeconds", Long.toString(DA_pollSeconds)));
@@ -115,77 +129,76 @@ public class Initiator  implements WurmServerMod, ServerPollListener, Configurab
     public void onServerPoll() {
         long second = 1000L;
 
-        if (useAlert0 && sysTime - second * Seconds0 > lastPoll0) {
+        if (useAlert0 && System.currentTimeMillis() - second * Seconds0 > lastPoll0) {
             RunAlert0();
-            lastPoll0 = sysTime;
+            lastPoll0 = System.currentTimeMillis();
         }
 
-        if (useAlert01 && sysTime - second * Seconds01 > lastPoll01) {
+        if (useAlert01 && System.currentTimeMillis() - second * Seconds01 > lastPoll01) {
             RunAlert01();
-            lastPoll01 = sysTime;
+            lastPoll01 = System.currentTimeMillis();
         }
 
-        if (useAlert02 && sysTime - second * Seconds02 > lastPoll02) {
+        if (useAlert02 && System.currentTimeMillis() - second * Seconds02 > lastPoll02) {
             RunAlert02();
-            lastPoll02 = sysTime;
+            lastPoll02 = System.currentTimeMillis();
         }
 
-        if (useAlert03 && sysTime - second * Seconds03 > lastPoll03) {
+        if (useAlert03 && System.currentTimeMillis() - second * Seconds03 > lastPoll03) {
             RunAlert03();
-            lastPoll03 = sysTime;
+            lastPoll03 = System.currentTimeMillis();
         }
 
-        if (useAlert04 && sysTime - second * Seconds04 > lastPoll04) {
+        if (useAlert04 && System.currentTimeMillis() - second * Seconds04 > lastPoll04) {
             RunAlert04();
-            lastPoll04 = sysTime;
+            lastPoll04 = System.currentTimeMillis();
         }
 
-        if (useAlert05 && sysTime - second * Seconds05 > lastPoll05) {
+        if (useAlert05 && System.currentTimeMillis() - second * Seconds05 > lastPoll05) {
             RunAlert05();
-            lastPoll05 = sysTime;
+            lastPoll05 = System.currentTimeMillis();
         }
 
-        if (useAlert06 && sysTime - second * Seconds06 > lastPoll06) {
+        if (useAlert06 && System.currentTimeMillis() - second * Seconds06 > lastPoll06) {
             RunAlert06();
-            lastPoll06 = sysTime;
+            lastPoll06 = System.currentTimeMillis();
         }
 
-        if (useAlert07 && sysTime - second * Seconds07 > lastPoll07) {
+        if (useAlert07 && System.currentTimeMillis() - second * Seconds07 > lastPoll07) {
             RunAlert07();
-            lastPoll07 = sysTime;
+            lastPoll07 = System.currentTimeMillis();
         }
 
-        if (useAlert08 && sysTime - second * Seconds08 > lastPoll08) {
+        if (useAlert08 && System.currentTimeMillis() - second * Seconds08 > lastPoll08) {
             RunAlert08();
-            lastPoll08 = sysTime;
+            lastPoll08 = System.currentTimeMillis();
         }
 
-        if (useAlert09 && sysTime - second * Seconds09 > lastPoll09) {
+        if (useAlert09 && System.currentTimeMillis() - second * Seconds09 > lastPoll09) {
             RunAlert09();
-            lastPoll09 = sysTime;
+            lastPoll09 = System.currentTimeMillis();
         }
 
-        if (useDeedAlert && sysTime - second * DA_pollSeconds > DA_lastPoll) {
+        if (useDeedAlert && System.currentTimeMillis() - second * DA_pollSeconds > DA_lastPoll) {
             RunDeedAlert();
-            DA_lastPoll = sysTime;
+            DA_lastPoll = System.currentTimeMillis();
         }
 
     }
 
     private void RunDeedAlert() {
         Village[] villages = Villages.getVillages();
-        // noinspection ForLoopReplaceableByForEach
-        for (int index = 0; index < villages.length; ++index) {
-            if (villages[index] != null) {
-                long lastLogout = Players.getInstance().getLastLogoutForPlayer(villages[index].getMayor().getId());
+        for (Village village : villages) {
+            if (village != null) {
+                long lastLogout = Players.getInstance().getLastLogoutForPlayer(village.getMayor().getId());
                 long deltaDay = System.currentTimeMillis() - lastLogout;
                 long days = TimeUnit.MILLISECONDS.toDays(deltaDay);
-                if (!villages[index].isPermanent && days >= MaxDays) {
-                    WriteLog(villages[index].getName() + " mayor " + villages[index].getMayor().getName() + " has not logged in for " + days + " days.");
-                    Server.getInstance().broadCastAlert(villages[index].getName() + " mayor " + villages[index].getMayor().getName() + " has not logged in for " + days + " days.", transmitTwitter);
+                if (!village.isPermanent && days >= MaxDays) {
+                    Server.getInstance().broadCastAlert(village.getName() + " mayor " + village.getMayor().getName() + " has not logged in for " + days + " days, village marked for disband.", false);
+                    WriteLog(village.getName() + " mayor " + village.getMayor().getName() + " has not logged in for " + days + " days, village marked for disband.");
                     if (autoDisband) {
-                        WriteLog(villages[index].getName() + " Disbanded by " + Servers.getLocalServerName() + " (Mayor NoLogin). " + " Mayor Name = " + villages[index].getMayor().getName() + ".");
-                        villages[index].disband(Servers.getLocalServerName() + " (Mayor NoLogin)");
+                        WriteLog(village.getName() + " Disbanded by " + Servers.getLocalServerName() + " (Mayor NoLogin). " + " Mayor Name = " + village.getMayor().getName() + ".");
+                        village.disband(Servers.getLocalServerName() + " (Mayor NoLogin)");
                     }
                 }
             }
@@ -194,9 +207,9 @@ public class Initiator  implements WurmServerMod, ServerPollListener, Configurab
 
     private void RunAlert0() {
         try {
-            InputStream input = new FileInputStream("mods/AutoAlerts.properties");
+            InputStream input = new FileInputStream(dir);
             prop.load(input);
-            Server.getInstance().broadCastAlert(prop.getProperty("Alert0"), transmitTwitter);
+            broadCastAlert(prop.getProperty("Alert0"), transmitTwitter);
         } catch (java.io.IOException e) {
             e.printStackTrace();
         }
@@ -204,9 +217,9 @@ public class Initiator  implements WurmServerMod, ServerPollListener, Configurab
 
     private void RunAlert01() {
         try {
-            InputStream input = new FileInputStream("mods/AutoAlerts.properties");
+            InputStream input = new FileInputStream(dir);
             prop.load(input);
-            Server.getInstance().broadCastAlert(prop.getProperty("Alert01"), transmitTwitter);
+            broadCastAlert(prop.getProperty("Alert01"), transmitTwitter);
         } catch (java.io.IOException e) {
             e.printStackTrace();
         }
@@ -214,9 +227,9 @@ public class Initiator  implements WurmServerMod, ServerPollListener, Configurab
 
     private void RunAlert02() {
         try {
-            InputStream input = new FileInputStream("mods/AutoAlerts.properties");
+            InputStream input = new FileInputStream(dir);
             prop.load(input);
-            Server.getInstance().broadCastAlert(prop.getProperty("Alert02"), transmitTwitter);
+            broadCastAlert(prop.getProperty("Alert02"), transmitTwitter);
         } catch (java.io.IOException e) {
             e.printStackTrace();
         }
@@ -224,9 +237,9 @@ public class Initiator  implements WurmServerMod, ServerPollListener, Configurab
 
     private void RunAlert03() {
         try {
-            InputStream input = new FileInputStream("mods/AutoAlerts.properties");
+            InputStream input = new FileInputStream(dir);
             prop.load(input);
-            Server.getInstance().broadCastAlert(prop.getProperty("Alert03"), transmitTwitter);
+            broadCastAlert(prop.getProperty("Alert03"), transmitTwitter);
         } catch (java.io.IOException e) {
             e.printStackTrace();
         }
@@ -234,9 +247,9 @@ public class Initiator  implements WurmServerMod, ServerPollListener, Configurab
 
     private void RunAlert04() {
         try {
-            InputStream input = new FileInputStream("mods/AutoAlerts.properties");
+            InputStream input = new FileInputStream(dir);
             prop.load(input);
-            Server.getInstance().broadCastAlert(prop.getProperty("Alert04"), transmitTwitter);
+            broadCastAlert(prop.getProperty("Alert04"), transmitTwitter);
         } catch (java.io.IOException e) {
             e.printStackTrace();
         }
@@ -244,9 +257,9 @@ public class Initiator  implements WurmServerMod, ServerPollListener, Configurab
 
     private void RunAlert05() {
         try {
-            InputStream input = new FileInputStream("mods/AutoAlerts.properties");
+            InputStream input = new FileInputStream(dir);
             prop.load(input);
-            Server.getInstance().broadCastAlert(prop.getProperty("Alert05"), transmitTwitter);
+            broadCastAlert(prop.getProperty("Alert05"), transmitTwitter);
         } catch (java.io.IOException e) {
             e.printStackTrace();
         }
@@ -254,9 +267,9 @@ public class Initiator  implements WurmServerMod, ServerPollListener, Configurab
 
     private void RunAlert06() {
         try {
-            InputStream input = new FileInputStream("mods/AutoAlerts.properties");
+            InputStream input = new FileInputStream(dir);
             prop.load(input);
-            Server.getInstance().broadCastAlert(prop.getProperty("Alert06"), transmitTwitter);
+            broadCastAlert(prop.getProperty("Alert06"), transmitTwitter);
         } catch (java.io.IOException e) {
             e.printStackTrace();
         }
@@ -264,9 +277,9 @@ public class Initiator  implements WurmServerMod, ServerPollListener, Configurab
 
     private void RunAlert07() {
         try {
-            InputStream input = new FileInputStream("mods/AutoAlerts.properties");
+            InputStream input = new FileInputStream(dir);
             prop.load(input);
-            Server.getInstance().broadCastAlert(prop.getProperty("Alert07"), transmitTwitter);
+            broadCastAlert(prop.getProperty("Alert07"), transmitTwitter);
         } catch (java.io.IOException e) {
             e.printStackTrace();
         }
@@ -274,9 +287,9 @@ public class Initiator  implements WurmServerMod, ServerPollListener, Configurab
 
     private void RunAlert08() {
         try {
-            InputStream input = new FileInputStream("mods/AutoAlerts.properties");
+            InputStream input = new FileInputStream(dir);
             prop.load(input);
-            Server.getInstance().broadCastAlert(prop.getProperty("Alert08"), transmitTwitter);
+            broadCastAlert(prop.getProperty("Alert08"), transmitTwitter);
         } catch (java.io.IOException e) {
             e.printStackTrace();
         }
@@ -284,14 +297,17 @@ public class Initiator  implements WurmServerMod, ServerPollListener, Configurab
 
     private void RunAlert09() {
         try {
-            InputStream input = new FileInputStream("mods/AutoAlerts.properties");
+            InputStream input = new FileInputStream(dir);
             prop.load(input);
-            Server.getInstance().broadCastAlert(prop.getProperty("Alert09"), transmitTwitter);
+            broadCastAlert(prop.getProperty("Alert09"), transmitTwitter);
         } catch (java.io.IOException e) {
             e.printStackTrace();
         }
     }
 
+    /**
+     * @param data What is logged.
+     */
     private void WriteLog(String data) {
         try {
             String logFile = "mods/AutoAlerts/log.txt";
@@ -304,5 +320,13 @@ public class Initiator  implements WurmServerMod, ServerPollListener, Configurab
         } catch (java.io.IOException e) {
             e.printStackTrace();
         }
+    }
+
+    /**
+     * @param data            What is being broadcast.
+     * @param transmitTwitter Weather or not data is transmitted to twitter.
+     */
+    private void broadCastAlert(String data, boolean transmitTwitter) {
+        Server.getInstance().broadCastAlert(data, transmitTwitter);
     }
 }
